0e57d6d692083f955211371b051fdf81cd6fb720
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2022-08-10 18:46:01 +0700

    Updated image size

--- script/latest.js
+++ script/latest.js
@@ -1,46 +1,46 @@
-var index = 0;
+var index = 0
 var loading = false
 
 $(document).ready(() => {
-    getlatest()
+	getlatest()
 })
 
 $(window).scroll(() => {
-    const scrollVal = $(window).scrollTop()
-    
-    if (Math.ceil(scrollVal) + $(window).height() === $(document).height()) {
-        if (loading) {
-            return
-        }
-        
-        loading = true
-        getlatest()
-    }
+	const scrollVal = $(window).scrollTop()
+
+	if (Math.ceil(scrollVal) + $(window).height() === $(document).height()) {
+		if (loading) {
+			return
+		}
+
+		loading = true
+		getlatest()
+	}
 })
 
-function getlatest(){
-    $.ajax({
-        type: 'POST',
-        url: 'api/getAllLatest.php',
-        data: {
-            index,
-            profileId
-        },
-        success: (res) => {
-            if(res.data.length > 0){
-                index = res.nextIndex
-            }
+function getlatest() {
+	$.ajax({
+		type: "POST",
+		url: "api/getAllLatest.php",
+		data: {
+			index,
+			profileId,
+		},
+		success: (res) => {
+			if (res.data.length > 0) {
+				index = res.nextIndex
+			}
 
-            res.data.forEach((e, idx) => {
-                var template = `<div class="image">
+			res.data.forEach((e, idx) => {
+				var template = `<div class="image">
                                     <img src="${e.image}" alt="">
                                 </div>`
 
-                var tag = $(template).appendTo("#images-container")
-                imageOverListener(tag, e)
-            })
+				var tag = $(template).appendTo("#images-container")
+				imageOverListener(tag, e)
+			})
 
-            loading = false
-        }
-    })
+			loading = false
+		},
+	})
 }
